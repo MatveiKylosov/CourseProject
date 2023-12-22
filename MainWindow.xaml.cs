8b39bb9e7e6f8a7d77838e89e8672ab7a281c622
MatveiKylosov <matveikyl@mail.ru>
2023-12-23 01:37:23 +0500

    MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace CourseProject
         public MainWindow()
         {
             InitializeComponent();
+            this.MouseDown += Window_MouseDown;
+
         }
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                Panel.Margin = new Thickness(0,5,0,0);
+                this.MaxHeight = SystemParameters.WorkArea.Height + 7;
+                this.MaxWidth = SystemParameters.WorkArea.Width + 20;
+            }
+            else if (this.WindowState == WindowState.Normal)
+            {
+                Panel.Margin = new Thickness(0, 0, 0, 0);
+                this.MaxHeight = double.PositiveInfinity;
+                this.MaxWidth = double.PositiveInfinity;
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void State_Click(object sender, RoutedEventArgs e) =>  Application.Current.MainWindow.WindowState = Application.Current.MainWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+        private void Minimize_Click(object sender, RoutedEventArgs e) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        private void Close_Click(object sender, RoutedEventArgs e) => this.Close();
+       
     }
 }
